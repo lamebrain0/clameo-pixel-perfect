6cbd69bf1be81f85dc8604cef2b55b09fd5b8cf8
Manish Kumar <manish.sspandey1@gmail.com>
2025-09-07 10:24:04 +0530

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,10 +101,21 @@ export default {
             height: "0",
           },
         },
+        "slide-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-in-right": "slide-in-right 0.3s ease-out",
       },
     },
   },
